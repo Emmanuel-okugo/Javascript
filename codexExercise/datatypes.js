2439ce794f449431e67fce0c274a812c8b5e954a
Emmanuel okugo <okugoemma@gmail.com>
2026-03-13 02:44:16 +0100

    starting datatypes

---
+++ codexExercise/datatypes.js
@@ -0,0 +1,19 @@
+/*
+Create some variables for your favorite company with the following data types:
+
+companyName, a string.
+foundingYear, a number.
+isActive, a Boolean.
+fundingAmount, undefined.
+Print them out using console.log().
+*/
+
+let companyName = "Telegram";
+let foundingYear = 2013;
+let isActive = true;
+let fundingAmount;
+
+console.log("comapny name:" + companyName);
+console.log("Founding year: " + foundingYear);
+console.log("is it still Active: " + isActive);
+console.log("Funding Amount: " + fundingAmount);
