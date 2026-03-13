865ae95283e73ce3c97501409cab76698a2f08a4
Emmanuel okugo <okugoemma@gmail.com>
2026-03-13 02:13:19 +0100

    learning var declaration with codex

---
+++ codexExercise/varDeclaration.js
@@ -0,0 +1,21 @@
+/*
+Create 4 variables for building a user profile on your favorite website:
+
+Two const variables for your first name and favorite color.
+Two let variables for your current location and mood.
+Lastly, print them out with console.log().
+
+Reassign a new value to at least one of the let variables and print it one more time with console.log().
+*/
+
+// assigning their values
+const firstName = "okugo";
+const favColor = "balck";
+let location = "Nsukka";
+let mood = "sad(i just saw my school result)";
+
+// printing them out in the console
+console.log("First Name: " + firstName);
+console.log("favorite color: " + favColor);
+console.log("Current Locaton: " + location);
+console.log("current mood: " + mood);
